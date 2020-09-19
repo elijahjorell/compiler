69a6c42112df1e3fab134b6479ed15ce6b84ffa1
elija <elijahesmero@gmail.com>
2020-09-19 12:26:45 +1000

    Find positions of curly brackets

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 var Network = require('./classes/network');
+var fs = require('fs');
 
-const science = new Network();
+// const science = new Network();
 
-science.add('physics');
-science.add('chemistry');
+// science.add('physics');
+// science.add('chemistry');
 
-science.relate('physics', 'is better than', 'chemistry')
+// science.relate('physics', 'is better than', 'chemistry')
 
-console.log(science.nodes.chemistry)
+// console.log(science.nodes.chemistry)
+
+var input = fs.readFileSync('./compiler-input.txt', 'utf-8');
+
+console.log(input
+    .split('\r')
+    .map((line) => line.trim())
+    .filter((line) => line != '')
+    .reduce((data, line, row) => {
+        line.split('').map((char, col) => {
+            if (char === '{' || char === '}') {
+                data.brackets.push({ type: char, row: row, col: col });
+            }
+        });
+        data.chunks.push(line);
+        return data
+    }, { chunks: [], brackets: [] })
+)
