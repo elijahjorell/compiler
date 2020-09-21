6d442556f1aff044bb7de8e19ab69d845e4209c4
elija <elijahesmero@gmail.com>
2020-09-21 12:27:09 +1000

    Try out universal-lexer

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+const UniversalLexer = require('universal-lexer/browser');
 const fs = require('fs');
+var input = fs.readFileSync('./compiler-input-2.txt', 'utf-8');
 
-var raw = fs.readFileSync('./compiler-input-2.txt', 'utf-8');
+const colon = {
+    type: 'Colon',
+    value: '{'
+};
 
-console.log(raw);
+definitions = [colon]
+
+const tokenise = UniversalLexer.compile(definitions);
+
+const tokens = tokenise(input).tokens;
+
+console.log(tokens);
